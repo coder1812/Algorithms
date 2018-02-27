0ae4ce9d67284580c389cfcd300d515e0428233a
coder1812 <deorohini1812@gmail.com>
2018-02-27 16:00:50 +0530

    commit

---
+++ src/com/bridgeit/programs/GenericBinarySearch.java
@@ -0,0 +1,38 @@
+package com.bridgeit.programs;
+
+import java.util.Scanner;
+import com.bridgeit.utility.Utility;
+
+public class GenericBinarySearch {
+	public static void main(String args[]) {
+		int choice;
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the type of array\n1-Integer\n2-String");
+		choice=scanner.nextInt();
+		switch(choice)
+		{
+		case 1: System.out.println("Enter the length of array");
+				int length1=scanner.nextInt();
+				Integer array1[]=new Integer[length1];
+				System.out.println("Enter the elements of array");
+				for(int i=0;i<length1;i++)
+				{	
+					array1[i]=scanner.nextInt();
+				}
+				Utility.genericBubbleSort(array1);
+		       break;
+		case 2: System.out.println("Enter the length of array");
+				int length2=scanner.nextInt();
+				String array2[]=new String[length2];
+				System.out.println("Enter the elements of array");
+				for(int j=0;j<length2;j++)
+				{
+					array2[j]=scanner.nextLine();
+				}
+				Utility.genericBubbleSort(array2);
+				break;
+		default:System.out.println("Error");		
+		}	
+		
+	}
+}
--- src/com/bridgeit/programs/QuestionToFindNumber.java
+++ src/com/bridgeit/programs/QuestionToFindNumber.java
@@ -6,39 +6,22 @@ import com.bridgeit.utility.Utility;
 public class QuestionToFindNumber {
 	public static void main(String args[])
 {
-		int result,number,j,i,target,count=0;
+		int result,number,target;
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter the number ");
 		number=scanner.nextInt();
 		System.out.println("Enter the target");
 		target=scanner.nextInt();
-		while(number>0)
-		{
-			if(number==1)
-			{
-				break;
-			}
-			else
-			{
-			   number=number/2;
-			   count++;
-			}
-		}
-		int array[]=new int[number];
-		for(i=0,j=1;i<number;i++,j++) {
-			array[i]=j;
-			
-		}
-		
-		result=Utility.questionToFindNumber(array,target,0,number-1,count);
+		result=Utility.questionToFindNumber(number, target);
 			if(result==-1)
 			{
 				System.out.println("Not found");
 			}
-			else {
+			else 
+			{
 				System.out.println("Found at index" +result);
 			}
-	scanner.close();	
+		scanner.close();	
 }
 	
 
